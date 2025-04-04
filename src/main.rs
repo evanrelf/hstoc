2ddf3c924e2a5d78926310312d0f0d71cbc68575
Evan Relf <evan@evanrelf.com>
2025-04-04 11:31:30 -0700

    Trivially parse Haskell code with Tree Sitter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser as _;
+use std::{fs, path::PathBuf};
+use tree_sitter::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+    path: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let mut parser = Parser::new();
+    parser.set_language(&tree_sitter_haskell::LANGUAGE.into())?;
+    let source_code = fs::read_to_string(&args.path)?;
+    let tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
+    println!("{}", root_node.to_sexp());
+    Ok(())
 }
