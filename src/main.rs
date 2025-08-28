d701cdbcb35e783f0016097afef8c7b5fadaf6be
Evan Relf <evan@evanrelf.com>
2025-08-28 16:42:51 -0700

    Fix lifetime elision warning

--- src/main.rs
+++ src/main.rs
@@ -75,11 +75,11 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn query_imports(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_imports(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(cx, "(haskell (imports (import module: (_) @import)))")
 }
 
-fn query_exports(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_exports(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     let explicit = query_explicit_exports(cx)?;
     if explicit.is_empty() {
         query_declarations(cx)
@@ -88,11 +88,11 @@ fn query_exports(cx: &Context) -> anyhow::Result<Vec<Node>> {
     }
 }
 
-fn query_explicit_exports(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_explicit_exports(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(cx, "(haskell (header (exports export: (_) @export)))")
 }
 
-fn query_declarations(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_declarations(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     let mut nodes = query_data_type(cx)?;
     nodes.extend(query_newtype(cx)?);
     nodes.extend(query_type_synonym(cx)?);
@@ -104,50 +104,50 @@ fn query_declarations(cx: &Context) -> anyhow::Result<Vec<Node>> {
     Ok(nodes)
 }
 
-fn query_data_type(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_data_type(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(
         cx,
         "(haskell (declarations (data_type name: (_) @data_type)))",
     )
 }
 
-fn query_newtype(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_newtype(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(cx, "(haskell (declarations (newtype name: (_) @newtype)))")
 }
 
-fn query_type_synonym(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_type_synonym(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(
         cx,
         "(haskell (declarations (type_synomym name: (_) @type_synonym)))",
     )
 }
 
-fn query_class(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_class(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(cx, "(haskell (declarations (class name: (_) @class)))")
 }
 
-fn query_type_family(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_type_family(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(
         cx,
         "(haskell (declarations (type_family name: (_) @type_family)))",
     )
 }
 
-fn query_function(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_function(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(
         cx,
         "(haskell (declarations (function name: (_) @function)))",
     )
 }
 
-fn query_function_infix(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_function_infix(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(
         cx,
         "(haskell (declarations (function (infix operator: (_) @function))))",
     )
 }
 
-fn query_bind(cx: &Context) -> anyhow::Result<Vec<Node>> {
+fn query_bind(cx: &Context) -> anyhow::Result<Vec<Node<'_>>> {
     query(cx, "(haskell (declarations (bind name: (_) @bind)))")
 }
 
